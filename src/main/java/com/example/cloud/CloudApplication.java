3a7c0ee7adbc80537738eb7af6b4b9342d7b498e
realmadred <liufeng2008168@163.com>
2017-10-12 17:23:19 +0800

    init

--- src/main/java/com/example/cloud/CloudApplication.java
+++ src/main/java/com/example/cloud/CloudApplication.java
@@ -2,10 +2,11 @@ package com.example.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class CloudApplication {
 
 	public static void main(String[] args) {
